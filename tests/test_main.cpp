6d7a6ed8507dafac8f9f5abee119549730654dbb
Ben Wilfong <bwilfong3@gatech.edu>
2026-03-06 21:01:18 -0500

    fix test suite

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,10 +1,19 @@
 #include <gtest/gtest.h>
 #include <mpi.h>
+#ifdef SIFV_HAS_PETSC
+#include <petsc.h>
+#endif
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
+#ifdef SIFV_HAS_PETSC
+    PetscInitialize(&argc, &argv, NULL, NULL);
+#endif
     ::testing::InitGoogleTest(&argc, argv);
     int result = RUN_ALL_TESTS();
+#ifdef SIFV_HAS_PETSC
+    PetscFinalize();
+#endif
     MPI_Finalize();
     return result;
 }
